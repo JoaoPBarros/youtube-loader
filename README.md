08792302d981171063cbcc9374ce001ce7a51d6d
JoaoPBarros <joaopedrobarros5@gmail.com>
2021-11-21 20:14:32 -0300

    add readme, license and oauth2.0

---
+++ README.md
@@ -0,0 +1,9 @@
+**WARNING**: This is not a ordinary react-app! This is a custom app and for that the scripts are diferents than usual. Please follow the steps.
+
+Inicialize using `yarn install` then `yarn dev` for a development environment.
+
+Use `yarn build` for a production environment.
+
+_This application may not function well with `npm` commands._
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import React, { Component } from 'react';
 import './styles/global.scss';
+import GoogleLogin from 'react-google-login';
 
-export function App() {
-  return <h1>Youtube - Loader</h1>
+export class App extends Component {
+  responseGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj);
+  }
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+          clientId="1061265429801-7uus4h7875pjo27adq3oh9m23lhh0kpe.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={ this.responseGoogle }
+          onFailure={ this.responseGoogle }
+          cookiePolicy={ 'single_host_origin' }
+        />
+      </div>
+    );
+  };
 }
+
+export default App;
