dd38b60c5b437f02da70ce6d1f0918fe53c7ab2e
JoaoPBarros <joaopedrobarros5@gmail.com>
2021-11-21 21:32:12 -0300

    creating and componentizing elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
+import GoogleSso from './components/google_login';
 import './styles/global.scss';
-import GoogleLogin from 'react-google-login';
 
-export class App extends Component {
-  responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
-  }
+class App extends Component {
   render() {
     return (
       <div>
-        <GoogleLogin
-          clientId="1061265429801-7uus4h7875pjo27adq3oh9m23lhh0kpe.apps.googleusercontent.com"
-          buttonText="Login"
-          onSuccess={ this.responseGoogle }
-          onFailure={ this.responseGoogle }
-          cookiePolicy={ 'single_host_origin' }
-        />
+        <GoogleSso />
       </div>
     );
   };
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input placeholder="O que você deseja procurar ?" />
+    );
+  }
+};
+
+export default SearchBar;
